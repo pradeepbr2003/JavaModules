e6f4c176decbe739224545ee0a9d9c1403a9e4e3
pradeepbr2003 <pradeepbr2003@gmail.com>
2025-04-30 11:32:25 +0530

    abstracted implementaion details

---
+++ MainModule/src/main/java/job/search/IMain.java
@@ -0,0 +1,31 @@
+package job.search;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static job.search.common.ExecuteJarEnum.*;
+
+public interface IMain {
+    Runnable singleTonRunnable = () -> executeJar(SINGLETON.command());
+    Runnable observerRunnable = () -> executeJar(OBSERVER.command());
+    Runnable java8Runnable = () -> executeJar(JAVA_STREAM.command());
+    Runnable liftProgramRunnable = () -> executeJar(LIFT_PROGRAM.command());
+    Runnable jobSeek = () -> executeJar(JOB_SEARCH.command());
+    Runnable parkingLot = () -> executeJar(PARKING_LOT.command());
+
+    private static void executeJar(String command) {
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+            BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            int exitCode = process.waitFor();
+            System.out.println("Exit Code: " + exitCode);
+        } catch (IOException | InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
--- MainModule/src/main/java/job/search/Main.java
+++ MainModule/src/main/java/job/search/Main.java
@@ -1,21 +1,8 @@
 package job.search;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import static job.search.common.ExecuteJarEnum.*;
-
-public class Main {
+public class Main implements IMain {
 
     public static void main(String[] args) {
-        Runnable singleTonRunnable = () -> executeJar(SINGLETON.command());
-        Runnable observerRunnable = () -> executeJar(OBSERVER.command());
-        Runnable java8Runnable = () -> executeJar(JAVA_STREAM.command());
-        Runnable liftProgramRunnable = () -> executeJar(LIFT_PROGRAM.command());
-        Runnable jobSeek = () -> executeJar(JOB_SEARCH.command());
-        Runnable parkingLot = () -> executeJar(PARKING_LOT.command());
-
         java8Runnable.run();
         observerRunnable.run();
         liftProgramRunnable.run();
@@ -24,18 +11,5 @@ public class Main {
         singleTonRunnable.run();
     }
 
-    private static void executeJar(String command) {
-        try {
-            Process process = Runtime.getRuntime().exec(command);
-            BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-            int exitCode = process.waitFor();
-            System.out.println("Exit Code: " + exitCode);
-        } catch (IOException | InterruptedException e) {
-            System.out.println(e.getMessage());
-        }
-    }
+
 }
